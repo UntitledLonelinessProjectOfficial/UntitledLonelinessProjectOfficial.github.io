49baf3dd3a541920e36ba7716cabe31898c0e80e
UntitledLonelinessProjectOfficial <mac@Macs-MacBook-Pro.local>
2016-06-08 09:45:49 +0800

    re added game

--- game/gamedev.js
+++ game/gamedev.js
@@ -4,6 +4,12 @@ var jodie = $("#jodie");
 $( "#jodie" ).hide();
 $( "#mob3" ).hide();
 $("#death").hide();
+$("#squid").hide();
+$("#mob4").hide();
+$("#mob5").hide();
+$("#mob6").hide();
+$("#mob7").hide();
+$("#mob8").hide();
 
 $("#refresh").hide();
 
@@ -14,11 +20,42 @@ $("#next").click(function(){
 	$("#jodie").show();
     $('#bulletleft').hide();
     $('#bulletright').hide();
-    $('#mob3').delay(1000).show();
+    $('#mob3').show();
     
-    });
+    });//end of #next click function
 
+$("#mob3").click(function(){
+    $( "#mob3" ).hide();
+    $( "#squid" ).show();
+});//end mob3 click
+
+$("#mob4").click(function(){
+    $( "#mob4" ).hide();
+    $( "#mob5" ).show();
 });
+
+$("#mob5").click(function(){
+    $( "#mob5" ).hide();
+    $( "#mob6" ).show();
+});
+
+$("#mob6").click(function(){
+    $( "#mob6" ).hide();
+    $( "#mob7" ).show();
+});
+
+$("#mob7").click(function(){
+    $( "#mob7" ).hide();
+    $( "#mob8" ).show();
+});
+
+$("#mob8").click(function(){
+    $( "#mob8" ).hide();
+    //BRUNO AND MYRON CHOOSE HOW YOUR GAME WILL END HERE!!
+});
+
+
+});//end document.ready
 
 
 $(document).keydown(function(e) {
@@ -50,23 +87,4 @@ $(document).keydown(function(e) {
     
     }
 });
-
-
-
-
-
-  
-});
-
-
-
-
-
-
- 
-  
-});
-
-
-
 
